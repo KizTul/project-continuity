02667b908e1210fbbbcb7fba750332efbcb06911
KizTul <kizylduktulek@gmail.com>
2025-08-25 17:58:24 +0300

    update cold start protocol for ARK v3.0

--- _ark_system/_tools/3_deploy_staging_to_production.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-chcp 65001 > nul
-echo. & echo [ДЕПЛОЙ ЯДРА] Развертываю изменения из песочницы в рабочую среду...
-python "%~dp0\deploy_staging_to_production.py"
--- _ark_system/_tools/modification_data.py
+++
@@ -1,8 +0,0 @@
-# modification_data.py
-modifications = [
-    {
-        "action": "CREATE_FILE_FROM_SOURCE",
-        "source_file": "_ark_system/_tools/patch_content.json",
-        "target_path": "ARK_EXPERIENCE_CODEX.json"
-    }
-]
